42b21b412df77bf3562ee43fef6d8ad6eb9bfcf8
Ramanika <ramanikadighade414@gmail.com>
2023-05-15 04:48:47 +0000

    gitignore added

--- demo.java
+++ demo.java
@@ -1,8 +1,11 @@
 public class Sample{
 	public static void main(String args[]){
-		System.out.println("Hello World !!");
+
+	System.out.println("Hello World !!");
+	System.out.println("Welcome to DevOps!");
 
 		//Hello World!!
+		//Welcome to DevOps!
 	}
 }
 
